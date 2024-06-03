76cfe11817ded589e0d88d64d0652015292bcd68
Doan Hau <haudd1705@gmail.com>
2024-06-03 10:26:39 +0700

    pussh

--- client/src/api/info.js
+++ client/src/api/info.js
@@ -3,7 +3,6 @@ import { getData } from '@lib/axios';
 export const getListUserInfoApi = (params) => getData('/info/getListUserInfo', params);
 export const getListApartmentInfoApi = (params) => getData('/info/getListApartmentInfo', params);
 export const getListDepartmentInfoApi = (params) => getData('/info/getListDepartmentInfo', params);
-export const getListApartmentInfoApi = (params) => getData('/info/getListApartmentInfo', params);
 export const getListApartmentGroupInfoApi = (params) => getData('/info/getListApartmentGroupInfo', params);
 export const getListPriceInfoApi = (params) => getData('/info/getListPriceInfo', params);
 export const getListServiceInfoApi = (params) => getData('/info/getListServiceInfo', params);
