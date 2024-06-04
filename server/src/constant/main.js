27ab467ec4176725859a469d338905b9d47b22c5
Doan Hau <haudd1705@gmail.com>
2024-06-04 11:01:25 +0700

    push

---
+++ server/src/constant/main.js
@@ -0,0 +1,10 @@
+export const VALIDATE_TYPE = {
+  EMAIL: 'email',
+  STRING: 'string',
+  NUMBER: 'number',
+  PHONE: 'phone',
+  JSON: 'json',
+  BOOLEAN: 'boolean',
+  DATE: 'date',
+  DATETIME: 'datetime'
+};
