cea7c4b7a5e8f4387848e0f78755ee12b67c822c
bachtv1509 <bachtv150902@gmail.com>
2024-07-19 13:07:39 +0700

    .

--- server/src/middleware/projectMiddleware.js
+++ server/src/middleware/projectMiddleware.js
@@ -8,11 +8,14 @@ export const projectMiddleware = async (req, res, next) => {
   try {
     const { project } = info;
     if (!project) return res.status(401).json({ status: false, mess: 'Dự án không hợp lệ!' });
-    const where = { status: 1, _id: project }
-    if (req.userInfo?.type !== "admin") where._id = { $in: req.userInfo.department?.projects };
-    const projectz = await detailProjectMd(where)
+    const where = { status: 1, _id: project };
+    if (req.userInfo?.type !== 'admin') {
+      const projects = req.userInfo.department?.projects;
+      if (!projects.includes(project)) return res.status(401).json({ status: false, mess: 'Bạn không có quyền quản lý dự án này!' });
+    }
+    const projectz = await detailProjectMd(where);
     if (!projectz) return res.status(401).json({ status: false, mess: 'Bạn không có quyền quản lý dự án này!' });
-    req.project = projectz
+    req.project = projectz;
     next();
   } catch (error) {
     res.status(500).json({ status: false, mess: error.toString() });
